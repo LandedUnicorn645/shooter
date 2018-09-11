d3b7b5f25daa5d8e8b3aa73c6137ab55a4fd1a01
Lorcan <lorcan@lorcan.mcveigh@gmail.com>
2018-09-11 20:05:04 +0100

    proper documentation on the server code and rewrote code to accomadate personal client objects

--- server.py
+++ server.py
@@ -1,7 +1,21 @@
 import pygame, socket, pickle
 
 class Server:
-    '''a simple server class that '''
+    '''Server class that connects multiple clients and checks and verifies
+        actions done by the users
+
+        Atrributes :
+            sock - the sock that connects to all the clients and listens for data
+            screenwidth - the width of the window that the game is on
+            screenheight - the height of the window that the game is on
+            clients - a list of client objects that are connected
+
+        Methods :
+            getClients - accepts the clients and creates a thread to listen for incoming data
+            recieveDate - for each client, listens for incoming data and verifies the data
+            checkMove - verifies the data sent if the client moves
+
+            '''
     def __init__(self):
         self.sock = socket(AF_INET, SOCK_STREAM)
         self.win = #window
@@ -14,11 +28,12 @@ class Server:
         self.sock.bind(server_addr)
         self.socket.listen(2)
         while len(self.clients) < 2:
-            client, client_Addr = socke.accpet()
-            self.clients.append((client,client_addr))
+            client, client_addr = socke.accpet()
+            client = Client(client, client_addr)
+            self.clients.append(client)
             #start a thread that listens for info from client
 
-    def recievingData(self, client, addr):
+    def recievingData(self, client):
         while True:
             recievedata = client.recv(pickle.loads(4096))
             if recievedata:
@@ -26,16 +41,15 @@ class Server:
                     self.checkMove(client, recievedata)
                 elif recievedata[0] == "shoot":
                     self.shoot(client, recievedata)
+                self.sendData(recievedata)
 
     def checkMove(self, client, data):
-        size = 40
+        size = client.size
         if data[1] == "x":
-            x = self.clients[client].x
-            if x >= 5 and x + size <= self.settings.screenwidth:
-                #update x value and move it
+            x = data[2]
+            if x >= 5 and x + size <= self.settings.screenwidth - 5:
+                client.x = x
         elif data[1] == "y":
-            y = self.clients[client].y
-            if y > 5 and y + size <= self.settings.screenheight:
-                #update y value and draw it
-
-    
+            y = data[2]
+            if y > 5 and y + size <= self.settings.screenheight - 5:
+                client.y = y
